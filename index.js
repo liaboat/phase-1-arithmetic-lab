6ef21cfdfd6d7704d9ec824d75e82f164bdf87b0
Lydia <lydia.boateng@student.moringaschool.com>
2022-06-10 15:33:49 +0100

    commit tree 388d087a227bb60aef4894350eb325f7a85646c8

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2; 
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10 )
+
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(0,2,5,10,15,20);
+
 
 // Write your code here
+
